90d75356c00c8bec4a5fa7caabab4f43f0cdaa96
HHC-1998 <haochenhou@LN.hk>
2026-04-14 10:35:57 +0800

    Update
    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './i18n/config';
 import './App.css';
 import Header from './components/Header';
